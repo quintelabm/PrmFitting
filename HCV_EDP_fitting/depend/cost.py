6737f0e49ebdf4ed45ecbfcb9b8a6cf29d5dc7bf
Matheus-Avila <matheusavila1997@gmail.com>
2020-05-14 13:32:37 -0300

    inclusao de EDPs no modelo para HCV

---
+++ HCV_EDP_fitting/depend/cost.py
@@ -0,0 +1,177 @@
+import numpy as np 
+from scipy.spatial import distance
+from scipy.interpolate import InterpolatedUnivariateSpline
+
+#Inicializacao
+
+#variaveis de inicializacao
+ageFim  = 50
+deltaA   = 0.1 #passo no age
+ageNpts = int(ageFim/deltaA ) + 1
+agePt = np.linspace(0, ageFim, ageNpts)
+ageCont = 0
+
+
+tempoFim = 2
+deltaT   = 0.01 #passo no tempo
+tempoNpts = int(tempoFim/deltaT) + 1 
+tempoPt = np.linspace(0, tempoFim, tempoNpts)
+tempoCont = 0
+
+#parametros
+s     = 1.3*10**5
+d     = 0.01
+beta = 5*10**-8
+c     = 22.30
+#parametro que sera ajustado 
+# delta = 0.62
+rho   = 8.180
+alpha = 30
+r     = 1.61
+
+
+k     = 0.80 # coeficiente da funcao exponencial de atraso na exportacao de RNA positivo
+tau   = 0.50 # tempo de atraso para a exportacao de RNA positivo
+Rmax  = 50.0 # numero maximo de RNA negativo / complexo de replicacao (Rn)
+sigma = 1.30 # taxa de formacao de complexos de replicacao
+mu_t  = 0.89 # 0.8 no artigo # decaimento natural de Rt
+theta = 1.20 # taxa de disponibilidade para traducao
+mu_c  = 2.39 # 0.89 no artigo # decaimento natural de Rc e Rn
+
+#variaveis 
+
+I = np.zeros((tempoNpts, ageNpts))
+Rp = np.zeros((tempoNpts, ageNpts))
+Rt = np.zeros((tempoNpts, ageNpts))
+Rn = np.zeros((tempoNpts, ageNpts))
+V = np.zeros(tempoNpts)
+T = np.zeros(tempoNpts)
+
+def calcIntegral(I,Rp,Rt):
+    soma = 0.0
+    for a in range(0, ageNpts):
+        soma = soma + I[a]*Rp[a]*Rt[a]
+    return soma/float(ageNpts)
+
+
+def diferencasfinitas(poi):
+    
+    #Solve
+    
+    epsilon_s     = poi[0]
+    epsilon_alpha = poi[1]
+    epsilon_r     = poi[2]
+    kappa_t       = poi[3]
+    kappa_c       = poi[4]
+    delta         = poi[5]
+
+    for t in range(1, tempoNpts):
+       
+        T[t] = (s - d*T[t-1] - beta*V[t-1]*T[t-1])*deltaT + T[t-1]
+        
+        V[t] =  deltaT*((1 - epsilon_s)*rho*calcIntegral(I[t-1],Rp[t-1],Rt[t-1]) 
+        - c*V[t-1]) + V[t-1]
+        
+        rho1 = 0.0
+        
+        Rp[t][0] = 0
+        Rn[t][0] = 0
+        Rt[t][0] = 1
+        I[t][0]  = beta*V[t]*T[t] 
+        
+        for a in range(1, ageNpts):
+            
+            if(float(a*deltaA) < tau):
+                rho1 = 0
+            else:
+                rho1 = rho*(1 - np.exp(-k*(float(a*deltaA) - tau)))
+                
+            I[t][a] = (-delta*I[t-1][a] - (I[t-1][a] - I[t-1][a-1])/deltaA)*deltaT + I[t-1][a]
+            
+            Rn[t][a] = ((1 - epsilon_r)*r*Rp[t-1][a]*(1 - (Rn[t-1][a]/Rmax)) - kappa_c*mu_c*Rn[t-1][a]
+                         - (Rn[t-1][a] - Rn[t-1][a-1])/(deltaA ))*deltaT + Rn[t-1][a]
+            
+            Rp[t][a] = ((1 - epsilon_alpha)*alpha*Rn[t-1][a] + sigma*Rt[t-1][a] - theta*Rp[t-1][a]
+    		            - (1- epsilon_s)*rho1*Rp[t-1][a] - kappa_c*mu_c*Rp[t-1][a]
+                        - (Rp[t-1][a] - Rp[t-1][a-1])/(deltaA))*deltaT + Rp[t-1][a]
+            
+            Rt[t][a] = (theta*Rp[t-1][a] - sigma*Rt[t-1][a] - (1 - epsilon_s)*rho1*Rt[t-1][a] - kappa_t*mu_t*Rt[t-1][a]
+                        - (Rt[t-1][a] - Rt[t-1][a-1])/(deltaA))*deltaT + Rt[t-1][a]
+    return V
+
+#recebe como parametro os parametros estocasticos, individuos, e os valores experimentais
+def viralmodelfit(poi, exp):
+    
+    #paramentros que serao ajustados 
+    # epsilon_s     = 0.998 # efetividade da terapia em diminuir ou bloquear a exportacao de RNA positivo
+    # epsilon_alpha = 0.924 # efetividade da terapia em diminuir ou bloquear a replicacao de RNA positivo
+    # epsilon_r     = 0.290 # efetividade da terapia em diminuir ou bloquear a replicacao de RNA negativo
+    # kappa_t       = 1.000 # fator para aumentar a degradacao de RNA positivo disponivel para traducao
+    # kappa_c       = 1.000 # fator para aumentar a degradacao de RNA positivo e negativo no complexo de replicacao
+    # delta = 0.62
+      
+    #condicoes iniciais
+    
+    T0 = 1.3*10**5
+    V0 = 5.45*pow(10,5);
+    
+    I0_t0 = beta*T0*V0
+    
+    Rt0_t0 = 1
+    Rp0_t0 = 0
+    Rn0_t0 = 0
+    
+    
+    T[0] = T0
+    V[0] = V0
+    
+    Rt[0][0] = Rt0_t0
+    Rp[0][0] = Rp0_t0
+    Rn[0][0] = Rn0_t0
+    I[0][0] = I0_t0
+    
+    rho1 = 0.00
+    
+    # Condicao inicial para as EDPs
+    for ageCont in range(1, ageNpts):
+        I[0][ageCont] = (beta*T[0]*V[0]*np.exp(-deltaA*ageCont))*deltaA + I[0][ageCont-1]
+        
+        if ageCont*deltaA < tau:
+            rho1 = 0
+        else:
+            rho1 = (1 - np.exp(-k*((ageCont*deltaA) - tau)))*rho
+        
+        Rn[0][ageCont] = (r*Rp[0][ageCont-1] - r*Rp[0][ageCont-1]*(Rn[0][ageCont-1]/Rmax) 
+        - mu_c*Rn[0][ageCont-1])*deltaA + Rn[0][ageCont-1]
+    
+        Rp[0][ageCont] = (alpha*Rn[0][ageCont-1] + sigma*Rt[0][ageCont-1] 
+        - theta*Rp[0][ageCont-1]- rho1*Rp[0][ageCont-1]
+        - mu_c*Rp[0][ageCont-1])*deltaA + Rp[0][ageCont-1]
+    
+        Rt[0][ageCont] = (theta*Rp[0][ageCont-1] - sigma*Rt[0][ageCont-1] 
+        - rho1*Rt[0][ageCont-1] - mu_t*Rt[0][ageCont-1])*deltaA + Rt[0][ageCont-1]
+        
+    #Fim das condicoess iniciais----------------------------------------------+
+    
+    # Executa o metedo numerico para resolver
+    # Precisa passar como parametro as condicoes iniciais 
+    diferencasfinitas(poi)
+    
+    # Passa para a base log o resultado
+    
+    V_log = np.log10(V)
+    
+    t_exp = [0, 0.083, 0.167, 0.25, 0.333, 0.5, 0.667, 1, 1.5, 2 ]
+    
+    # fazer interpolacao usando os pontos experimentais
+    ius = InterpolatedUnivariateSpline(t_exp, exp)
+    
+    # aplicar a funcao interpolada nos pontos do metodo numerico do modelo
+    yi = ius(tempoPt)
+    
+    # calcular a distancia euclidiana entre os resultados experimentais interpolados
+    # e o resultado do modelo
+    
+    dst = distance.euclidean(V_log, yi)
+    
+    return dst
---
+++ HCV_EDP_fitting/differential_evolution.py
@@ -0,0 +1,174 @@
+# Tutorial - https://nathanrooy.github.io/posts/2017-08-27/simple-differential-evolution-with-python/
+#
+#1) Initialize a random population of individuals throughout the search space.
+#2) while iter <= max num of generations
+#    3) cycle through each individual in the population   
+#        3.A) perform mutation            
+#        3.B) perform recombination ("crossover" in GA lingo)            
+#        3.C) perform selection           
+#    4) if stopping criterion has been met:
+#            exit and return best individual            
+#        else:
+#            iter = iter + 1
+#            go back to step #3
+
+#------------------------------------------------------------------------------+
+#
+#   Nathan A. Rooy
+#   A simple, bare bones, implementation of differential evolution with Python
+#   August, 2017
+#
+#------------------------------------------------------------------------------+
+
+#--- IMPORT DEPENDENCIES ------------------------------------------------------+
+
+import random
+import numpy as np
+from depend.cost import viralmodelfit
+from depend.bounds import ensure_bounds
+
+
+#--- MAIN ---------------------------------------------------------------------+
+
+def main(cost_func, bounds, popsize, mutate, recombination, maxiter, POI, PAT):
+
+    #--- INITIALIZE A POPULATION (step #1) ----------------+
+    
+    population = []
+    for i in range(0,popsize):
+        indv = []
+        for j in range(len(bounds)):
+            indv.append(random.uniform(bounds[j][0],bounds[j][1]))
+        population.append(indv)
+            
+    #--- SOLVE --------------------------------------------+
+
+    # cycle through each generation (step #2)
+    for i in range(1,maxiter+1):
+        print('GENERATION:',i)
+
+        gen_scores = [] # score keeping
+
+        # cycle through each individual in the population
+        for j in range(0, popsize):
+
+            #--- MUTATION (step #3.A) ---------------------+
+            
+            # select three random vector index positions [0, popsize), not including current vector (j)
+            candidates = list(range(0,popsize))
+            #candidates.remove(j)
+            del candidates[j]
+            random_index = random.sample(candidates, 3)
+
+            x_1 = population[random_index[0]]
+            x_2 = population[random_index[1]]
+            x_3 = population[random_index[2]]
+            x_t = population[j]     # target individual
+
+            # subtract x3 from x2, and create a new vector (x_diff)
+            x_diff = [x_2_i - x_3_i for x_2_i, x_3_i in zip(x_2, x_3)]
+
+            # multiply x_diff by the mutation factor (F) and add to x_1
+            v_donor = [x_1_i + mutate * x_diff_i for x_1_i, x_diff_i in zip(x_1, x_diff)]
+            v_donor = ensure_bounds(v_donor, bounds)
+
+            #--- RECOMBINATION (step #3.B) ----------------+
+
+            v_trial = []
+            for k in range(len(x_t)):
+                crossover = random.random()
+                if crossover <= recombination:
+                    v_trial.append(v_donor[k])
+
+                else:
+                    v_trial.append(x_t[k])
+                    
+            #--- GREEDY SELECTION (step #3.C) -------------+
+
+            score_trial  = cost_func(v_trial, PAT)
+            score_target = cost_func(x_t, PAT)
+
+            if score_trial < score_target:
+                population[j] = v_trial
+                gen_scores.append(score_trial)
+                print('   >',score_trial, v_trial)
+
+            else:
+                print('   >',score_target, x_t)
+                gen_scores.append(score_target)
+
+        #--- SCORE KEEPING --------------------------------+
+
+        gen_avg = sum(gen_scores) / popsize                         # current generation avg. fitness
+        gen_best = min(gen_scores)                                  # fitness of best individual
+        gen_sol = population[gen_scores.index(min(gen_scores))]     # solution of best individual
+
+        print('      > GENERATION AVERAGE:',gen_avg)
+        print('      > GENERATION BEST:',gen_best)
+        print('         > BEST SOLUTION:',gen_sol,'\n')
+
+    return gen_sol
+
+#--- CONSTANTS ----------------------------------------------------------------+
+
+cost_func = viralmodelfit                                  # Cost function
+bounds = [(0.9,0.99),(0.9,0.99),(0.2,0.5),(1.0,3.0),(1.0,3.0),(0.5,1.2)]  # Bounds [(x1_min, x1_max), (x2_min, x2_max),...]
+popsize = 10                                               # Population size, must be >= 4
+mutate = 0.5                                               # Mutation factor [0,2]
+recombination = 0.7                                        # Recombination rate [0,1]
+maxiter = 10                                               # Max number of generations (maxiter)
+
+
+# --- for patient B07
+#PATB07= [4.2227, 4.0733, 3.0599, 2.2122, 1.7924, 1.6435, 1.7160, 1.6435, 1.2304, 1.0792, 1.0000, 1.0000, 1.0000, 1.0000]
+#PAT = PATB07
+
+# --- for patient B09
+#PATB09 = [6.2734, 6.0103, 5.9685, 5.6755, 5.5739, 5.4454, 4.9222, 5.1703, 4.8385, 4.1949, 3.0931, 2.1790, 1.5682, 1.5051]
+#PAT = PATB09
+
+# --- for patient B08
+#PATB08 = [5.6546, 5.6486, 4.6203, 3.6876, 3.1526, 2.6355, 2.5302, 2.6294, 2.2788, 2.0719, 1.9031, 1.7924, 1.6335, 1.5682]
+#PAT = PATB08
+
+# --- for patient B16
+#PATB16 = [6.3541, 6.3212, 5.5915, 4.1949, 3.8517, 3.6651, 3.4814, 3.2529, 3.0120, 3.0302, 2.7528, 2.3838, 2.1818, 1.9243]
+#PAT = PATB16
+
+# --- for patient B17
+#PATB17 = [6.4885, 6.5486, 5.6096, 4.4266, 3.8878, 3.2076, 3.1626, 2.9128, 2.8432, 2.7474, 2.7016, 2.3541, 2.0453, 1.4914]
+#PAT = PATB17
+
+# --- for patient B06
+#PATB06 = [6.3780, 6.4109, 5.6277, 4.4948, 3.9268, 3.1973, 2.8537, 2.5340, 2.4378, 2.3404, 2.3345, 2.2355, 2.0492, 2.1173]
+#PAT = PATB06
+
+# --- for patient C05
+#PATC05 = [6.394490, 6.254302, 5.833741, 4.727484, 3.889414, 3.261501, 3.182415, 2.990339, 2.609594, 2.527630, 2.743510, 2.694605, 2.227887, 1.863323]
+#PAT = PATC05
+
+# --- for patient C06
+#PATC06 = [6.839431, 6.735814, 6.452393, 5.340039, 4.581198, 3.481012, 3.164055, 2.780317, 2.484300, 2.509203, 2.369216, 1.949390, 1.623249, 1.556303]
+#PAT = PATC06
+
+# --- for patient C09
+#PATC09 = [6.424965, 6.303063, 6.028728, 4.642148, 4.349588, 3.484015, 3.243286, 2.816904, 2.576341, 2.089905, 2.025306, 1.698970, 1.278754, 1.342423]
+#PAT = PATC09
+
+# --- for patient C10
+PATC10 = [5.583842, 5.455846, 4.754914, 3.447468, 2.749736, 2.311754, 2.158362, 1.944483, 1.707570, 1.322219, 1.322219, 1.000000, 1.000000, 1.000000]
+#PAT = PATC10
+
+PAT83 = [5.45, 5.38, 4.73, 4.00, 3.39, 2.89, 2.68, 2.72, 2.97, 1.93]
+PAT = PAT83
+# initial guess for parameters
+# delta epsilon p c
+POI = [0.08, 0.998, 1.0, 20.0]
+
+#--- RUN ----------------------------------------------------------------------+
+
+if __name__ == "__main__":
+    main(cost_func, bounds, popsize, mutate, recombination, maxiter, POI, PAT)
+
+#--- END ----------------------------------------------------------------------+
+
