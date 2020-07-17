880b47097b42190e862f0bb2e900ce87c998a918
jessica-garbero <46576343+jessica-garbero@users.noreply.github.com>
2020-07-17 19:19:23 -0300

    Separação de arquivos
    Dentro da pasta Cluster foram criadas 10 pastas separadas, cada uma com o código pronto para executar o ajuste de parâmetros para cada paciente especificado.

---
+++ HCV_DE_scipy_long_term/Tests/Cluster/B09/depend/bounds.py
@@ -0,0 +1,21 @@
+#--- FUNCTIONS ----------------------------------------------------------------+
+
+def ensure_bounds(vec, bounds):
+
+    vec_new = []
+    # cycle through each variable in vector 
+    for i in range(len(vec)):
+
+        # variable exceedes the minimum boundary
+        if vec[i] < bounds[i][0]:
+            vec_new.append(bounds[i][0])
+
+        # variable exceedes the maximum boundary
+        if vec[i] > bounds[i][1]:
+            vec_new.append(bounds[i][1])
+
+        # the variable is fine
+        if bounds[i][0] <= vec[i] <= bounds[i][1]:
+            vec_new.append(vec[i])
+        
+    return vec_new
