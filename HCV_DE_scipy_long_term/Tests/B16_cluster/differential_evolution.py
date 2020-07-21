a3126bc81604cf2909f7775b232bfad0f091219f
barbarademelo <barbara.demelo2@unibo.it>
2020-07-21 18:28:28 +0200

    update folder B16_cluster - errors function call

--- HCV_DE_scipy_long_term/Tests/B16_cluster/differential_evolution.py
+++ HCV_DE_scipy_long_term/Tests/B16_cluster/differential_evolution.py
@@ -37,7 +37,7 @@ from scipy.optimize import differential_evolution
 cost_func = viralmodelfit                                  # Cost function
 bounds = [(0.09,0.99),(0.09,0.99),(0.09,0.99),(0.01,1.2),(25,35),(0.9,2.0),(6,12)]  # Bounds [(x1_min, x1_max), (x2_min, x2_max),...]
 popsize = 10                                               # Population size, must be >= 4
-mutate = 0.5                                               # Mutation factor [0,2]
+mutation = 0.5                                               # Mutation factor [0,2]
 recombination = 0.7                                        # Recombination rate [0,1]
 maxiter = 10                                              # Max number of generations (maxiter)
 
@@ -68,7 +68,7 @@ t_exp = np.array([0.04, 0.09, 0.17, 0.34, 0.50, 1.00, 1.50, 2.98, 4.91, 6.92, 10
 #--- RUN ----------------------------------------------------------------------+
 
 if __name__ == "__main__":
-    saida = open("output/relatorio.txt", "a+")
+    saida = open("HCV_DE_scipy_long_term/Tests/B16_cluster/output/relatorio.txt", "a+")
     saida.writelines("\n\n-------NOVA TENTATIVA-------\n\n")
     saida.writelines('Population size: '+ str(popsize)+ '\nNumber of generations: '+ str(maxiter)+ '\n')
 
@@ -78,7 +78,7 @@ if __name__ == "__main__":
         print(pat_cont, " Patient")
         saida.writelines(str(pat_cont) + " Patient\n\n")
         
-        sol_pat = differential_evolution(cost_func, bounds, args=(pat,10**pat[0]), maxiter=50, popsize=50, mutation=0.5, recombination=0.7,updating='deferred',workers=-1)
+        sol_pat = differential_evolution(cost_func, bounds, args=(pat,10**pat[0]), maxiter=maxiter, popsize=popsize, mutation=mutation, recombination=recombination, updating='deferred', workers=8)
         #sol_pat.x => parametros--- sol_pat.fun => custo/ retorno da cost.py
         print(sol_pat.x, "\n", sol_pat.fun)
         saida.writelines('\nCusto do melhor conjunto de parametros: '+ str(sol_pat.fun) +'\n\n')
@@ -88,7 +88,7 @@ if __name__ == "__main__":
         plt.plot(t_exp, pat, 'ro')
         #Plot da solucao com a media dos parametros
         cost_func(sol_pat.x, pat, (10**pat[0]))
-        plt.savefig("output/figura"+str(pat_cont)+".png")
+        plt.savefig("./output/figura"+str(pat_cont)+".png")
         
         pat_cont += 1
 
