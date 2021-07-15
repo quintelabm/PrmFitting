fd10c2d1265f013fe4e43477cfb8fec0e0430932
Matheus-Avila <matheusavila1997@gmail.com>
2021-07-15 13:52:55 -0300

    DE 30 dias -> plot 70 dias

--- codigo_C++lt5/differential_evolution/depend/HCV_Model.h
+++ codigo_C++lt5/differential_evolution/depend/HCV_Model.h
@@ -150,7 +150,7 @@ void HCV_Model::initialize(){
     /**
     * number of days simulated
     */
-    days      = 74;
+    days      = 35;
     /**
     * number of points saved
     */
---
+++ relatório/relatorio 07-15-21/calculo_mean_std.py
@@ -0,0 +1,70 @@
+import numpy as np
+a = [0.9816 ,0.1091, 0.1323 , 0.2218, 0.5221,  0.2919]
+b = [0.8405,0.5662, 0.8528 , 0.1919, 0.7155, 0.8322]
+c = [0.9819,0.1824, 0.6449, 0.9837, 0.9717, 0.9864]
+d = [21.56, 23.3863, 32.879 , 20.375, 53.967, 44.423]
+e = [ 4.896, 0.1085, 5.8272 , 3.503, 3.498, 2.7514]
+f = [0.4134, 0.1930, 0.1522, 0.8648, 0.9946, 1.4732]
+g = [0.3347, 0.1695, 0.5341, 1.055, 1.2674, 0.4375]
+h = [13.89 , 3.163, 12.85, 13.861, 11.82,  7.531]
+i = [1.700, 1.5829, 1.0384, 1.7541, 1.202, 1.5367]
+j = [1.767, 1.3799, 1.5918, 1.9221, 1.7683, 1.3848]
+k = [11.18, 10.2876, 12.548, 11.24, 10.0024, 10.08]
+
+# print(np.mean(a))
+# print(np.std(a))
+# print(np.mean(b))
+# print(np.std(b))
+# print(np.mean(c))
+# print(np.std(c))
+# print(np.mean(d))
+# print(np.std(d))
+# print(np.mean(e))
+# print(np.std(e))
+# print(np.mean(f))
+# print(np.std(f))
+# print(np.mean(g))
+# print(np.std(g))
+# print(np.mean(h))
+# print(np.std(h))
+# print(np.mean(i))
+# print(np.std(i))
+# print(np.mean(j))
+# print(np.std(j))
+# print(np.mean(k))
+# print(np.std(k))
+
+a = [0.554 , 0.227, 0.286, 0.914]
+b = [0.812, 0.966, 0.8396 , 0.926]
+c = [0.198, 0.757, 0.122, 0.958]
+d = [28.295, 30.85, 43.97, 25.13]
+e = [6.82, 4.62, 1.444, 2.122]
+f = [0.227, 0.225, 0.695, 0.228]
+g = [0.119, 0.317, 0.22, 0.264]
+h = [11.3, 9.47, 5.17, 10.85]
+i = [1.854, 1.25, 1.694, 1.236]
+j = [1.44, 1.74, 1.32, 1.894]
+k = [10, 10, 10, 11.83]
+
+print(np.mean(a))
+print(np.std(a))
+print(np.mean(b))
+print(np.std(b))
+print(np.mean(c))
+print(np.std(c))
+print(np.mean(d))
+print(np.std(d))
+print(np.mean(e))
+print(np.std(e))
+print(np.mean(f))
+print(np.std(f))
+print(np.mean(g))
+print(np.std(g))
+print(np.mean(h))
+print(np.std(h))
+print(np.mean(i))
+print(np.std(i))
+print(np.mean(j))
+print(np.std(j))
+print(np.mean(k))
+print(np.std(k))
